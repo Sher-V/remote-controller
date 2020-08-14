5323107f257ab6fa06ff1726ca63e7e257bac793
Vadim Sheremetov <beingalwaysrightthere@gmail.com>
2020-08-14 12:43:05 +0300

    backup

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.ATlAS_URI, {
+mongoose.connect('mongodb://127.0.0.1:27017/remote-controller', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,
